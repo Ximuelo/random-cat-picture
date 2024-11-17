cfe0512bd0f46e7604fbcb9641ccde4cbb74bc93
Ximuelo <xquintanaf.denia@gmail.com>
2024-11-17 16:39:02 +0100

    Refresh button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'3xl': '0 90px 140px -40px rgba(0, 0, 0, 0.3)',
+			}
+		},
 	},
 	plugins: [],
 }
